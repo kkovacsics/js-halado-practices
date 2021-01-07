5851aaca737f496f89a9ec8483f4474d2ea4acc9
kkovacsics <kalman.kovacsics@gmail.com>
2021-01-07 16:37:08 +0100

    ESLint használat

--- tasks/js/task.js
+++ tasks/js/task.js
@@ -1,40 +1,57 @@
 /* 1. feladat
-Hozz létre egy html fájlt benne 3 gombbal! A gombok szövegei az alábbiak legyenek: Első, Második, Harmadik. 
+Hozz létre egy html fájlt benne 3 gombbal!
+A gombok szövegei az alábbiak legyenek: Első, Második, Harmadik.
 Mindegyik gombnak add meg a button class-t!
-Készíts egy JavaScript fájlt, és linkeld be a html oldaladba! 
-Írj egy függvény kifejezést arrow function segítségével. 
-A függvény neve handleClick legyen A függvény a meghívása után mindegyik gombhoz hozzáad egy eseményfigyelőt, 
-amely kattintásra kiírja a gomb szövegét a konzolra. 
-A függvényen belül ciklust használj, tehát ne manuálisan egyesével add a gombokhoz az eseményfigyelőt!
+Készíts egy JavaScript fájlt, és linkeld be a html oldaladba!
+Írj egy függvény kifejezést arrow function segítségével.
+A függvény neve handleClick legyen.
+A függvény a meghívása után mindegyik gombhoz hozzáad egy eseményfigyelőt,
+amely kattintásra kiírja a gomb szövegét a konzolra.
+A függvényen belül ciklust használj,
+tehát ne manuálisan egyesével add a gombokhoz az eseményfigyelőt!
  */
 const handleClick = () => {
-    const buttons = document.querySelectorAll('button');
-    for(let i=0; i<buttons.length; i++)
-        buttons[i].addEventListener('click', function(){
-            console.log(this.textContent);
-        });
+  const buttons = document.querySelectorAll('button');
+  for (let i = 0; i < buttons.length; i += 1) {
+    buttons[i].addEventListener('click', function handler() {
+      console.log(this.textContent);
+    });
+  }
 };
 handleClick();
 
 /* 2. feladat
-Hozz létre két függvény kifejezést arrow functionnel! A függvények neve summation és subtraction. 
-Mindegyik függvény két paramétert kap, ezek neve a és b. 
-Amennyiben bármelyik paraméter hiányozna a függvény meghívásakor, úgy az alapértelemezett érték a 0 legyen.
+Hozz létre két függvény kifejezést arrow functionnel! A függvények neve summation és subtraction.
+Mindegyik függvény két paramétert kap, ezek neve a és b.
+Amennyiben bármelyik paraméter hiányozna a függvény meghívásakor,
+úgy az alapértelemezett érték a 0 legyen.
 A summation visszatér a két szám összegével, a subtraction visszatér a két szám különbségével.
  */
-const summation = (a=0, b=0) => a+b;
-const subtraction  = (a=0, b=0) => a-b;
-
+const summation = (a = 0, b = 0) => a + b;
+const subtraction = (a = 0, b = 0) => a - b;
+summation(10);
+subtraction(10, 2);
 /* 3. feladat
-Hozz létre egy függvény kifejezést arrow functionnel! A függvény neve personDataLog legyen. 
-A függvény paraméterként egy objektumot vár, ami egy firstname, lastname, és age tulajdonságokkal rendelkezik. 
-Amennyiben bármelyik tulajdonság hiányozna úgy rendre a következő alapértelemezett értékeket vegyék fel: 
+Hozz létre egy függvény kifejezést arrow functionnel! A függvény neve personDataLog legyen.
+A függvény paraméterként egy objektumot vár,
+ami egy firstname, lastname, és age tulajdonságokkal rendelkezik.
+Amennyiben bármelyik tulajdonság hiányozna,
+úgy rendre a következő alapértelemezett értékeket vegyék fel:
 - firstname: John - lastname: Doe - age: 33
 
 A függvény kiírja az adott illető adatai a konzolra az adott formátumban:
 My name is firstname, lastname. I'm age years old.
-A firstname, lastname és age helyére a paraméterként kapott objektum tulajdonságok értékét kell behelyettesíteni.
+A firstname, lastname és age helyére
+a paraméterként kapott objektum tulajdonságok értékét kell behelyettesíteni.
  */
-const personDataLog = ({firstname='John', lastname='Doe', age=33}={}) => {
-    console.log(`My name is ${firstname}, ${lastname}. I\'m ${age} years old.`);
-}
+const personDataLog = ({ firstname = 'John', lastname = 'Doe', age = 33 } = {}) => {
+  console.log(`My name is ${firstname}, ${lastname}. I'm ${age} years old.`);
+};
+personDataLog({ firstname: 'Jane' });
+
+// strict mode?
+(function strict() {
+  // 'use strict';
+
+  console.log(this);
+}());
